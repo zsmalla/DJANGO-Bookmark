8f3145dd729afa9b9e840c489b055e36ed8eef83
{zsmalla} <{malla7454@gmail.com}>
2022-03-09 19:41:22 +0900

    Detail View

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BookmarkListView, BookmarkCreateView
+from .views import BookmarkListView, BookmarkCreateView, BookmarkDetailView
 
 urlpatterns =[
     path("", BookmarkListView.as_view(), name='list'),   # 클래스형 뷰 -> 함수형 뷰로 변환 : as_view()
     path("add/", BookmarkCreateView.as_view(), name='add'),
+    path("detail/<int:pk>/", BookmarkDetailView.as_view(), name='detail'),
 ]
