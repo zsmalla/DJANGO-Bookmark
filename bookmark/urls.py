d38b6e94b2f4db98fbd32c1c04ab60b363dffa95
{zsmalla} <{malla7454@gmail.com}>
2022-03-09 19:19:26 +0900

    CreateView

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BookmarkListView
+from .views import BookmarkListView, BookmarkCreateView
 
 urlpatterns =[
     path("", BookmarkListView.as_view(), name='list'),   # 클래스형 뷰 -> 함수형 뷰로 변환 : as_view()
+    path("add/", BookmarkCreateView.as_view(), name='add'),
 ]
--- bookmark/views.py
+++ bookmark/views.py
@@ -8,7 +8,15 @@ from django.shortcuts import render
 # 웹 페이지에 접속한다. -> 페이지를 본다.
 # URL을 입력 -> 웹 서버가 뷰를 찾아서 동작시킨다. -> 응답
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Bookmark
 
 class BookmarkListView(ListView):
     model = Bookmark
+
+class BookmarkCreateView(CreateView):
+    model = Bookmark
+    fields = ['site_name', 'url'] # 어떤 내용을 추가할 것인지? 없으면 오류(ImproperlyConfigured)!
+    success_url = reverse_lazy('list')    # 전송 요청 성공 시 리로드 되는 페이지
+    template_name_suffix = '_create' # 기본적으로 CreateView, UpdateView의 템플릿 이름은 ..._form인데 이걸 임의로 바꿔줌
